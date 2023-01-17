1d7b761d72e69beda89a63d68b109452bf132d63
Cletus Kingdom <cletuskingdom@gmail.com>
2023-01-17 17:29:50 +0100

    i used Stateless Functional Component in the navbar component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,18 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-	render() {
-		return (
-			<nav class="navbar fixed-top bg-dark" data-bs-theme="dark">
-				<div className="container-fluid">
-					<a className="navbar-brand text-light" href="#">
-						NavBar -{" "}
-						<span className="badge bg-secondary">
-							{this.props.totalCounters}
-						</span>
-					</a>
-				</div>
-			</nav>
-		);
-	}
-}
+const NavBar = (props) => {
+	return (
+		<nav class="navbar fixed-top bg-dark" data-bs-theme="dark">
+			<div className="container-fluid">
+				<a className="navbar-brand text-light" href="#">
+					NavBar -{" "}
+					<span className="badge bg-secondary">
+						{props.totalCounters}
+					</span>
+				</a>
+			</div>
+		</nav>
+	);
+};
 
 export default NavBar;
