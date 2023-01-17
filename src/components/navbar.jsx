50efa1523abdc836514b01b5e0500289a0f297cb
Cletus Kingdom <cletuskingdom@gmail.com>
2023-01-17 17:32:29 +0100

    i implemented Destructuring

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from "react";
 
-const NavBar = (props) => {
+const NavBar = ({ totalCounters }) => {
 	return (
 		<nav class="navbar fixed-top bg-dark" data-bs-theme="dark">
 			<div className="container-fluid">
 				<a className="navbar-brand text-light" href="#">
 					NavBar -{" "}
-					<span className="badge bg-secondary">
-						{props.totalCounters}
-					</span>
+					<span className="badge bg-secondary">{totalCounters}</span>
 				</a>
 			</div>
 		</nav>
