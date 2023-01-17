d5345e3eea523897d13b1dc08c080df50fd0d9f7
Cletus Kingdom <cletuskingdom@gmail.com>
2023-01-17 16:29:54 +0100

    delete btn working

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -12,7 +12,8 @@ class Counters extends Component {
 	};
 
 	handleDelete = (counterId) => {
-		console.log("event handle called", counterId);
+		const counters = this.state.counters.filter((c) => c.id != counterId);
+		this.setState({ counters });
 	};
 
 	render() {
